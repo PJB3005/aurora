49957f24eb2fcd6d8302298b9314c938f31b38ab
PJB3005 <pieterjan.briers+git@gmail.com>
2026-05-12 01:10:22 +0200

    libpng instead of spng

--- lib/gfx/png_io.cpp
+++ lib/gfx/png_io.cpp
@@ -3,73 +3,105 @@
 #include <fstream>
 
 #include "dds_io.hpp"
-#include "spng.h"
+#include "png.h"
+#include "../fs_helper.hpp"
 
 static aurora::Module Log("aurora::gfx::png");
 
 namespace aurora::gfx::png {
 
-struct SpngContext {
-  spng_ctx* ctx;
-  explicit SpngContext(spng_ctx* ctx) : ctx(ctx) { }
-  ~SpngContext() {
-    if (ctx) {
-      spng_ctx_free(ctx);
-    }
-  }
-  operator spng_ctx*() const {
-    return ctx;
+struct PngStructs {
+  png_structp pStruct;
+  png_infop pInfo;
+  std::ifstream file;
+
+  ~PngStructs() {
+    png_destroy_read_struct(&pStruct, &pInfo, nullptr);
   }
 };
 
+static void readPngData(png_structp png, png_bytep data, const size_t length) {
+  auto structs = static_cast<PngStructs*>(png_get_io_ptr(png));
+  structs->file.read(reinterpret_cast<char*>(data), static_cast<std::streamsize>(length));
+
+  if (structs->file.eof() || structs->file.fail()) {
+    png_error(png, "file read failed!");
+  }
+}
+
 std::optional<ConvertedTexture>
 load_png_file(const std::filesystem::path& path) noexcept {
-  const auto bytes = dds::read_binary_file(path);
-  if (!bytes.has_value()) {
+  PngStructs structs{};
+
+  if (path.filename() == "tex1_608x448_cf479087a3a9a3e0_6.png") {
+    Log.error("failed to open file: {}", fs_path_to_string(path));
+
+  }
+
+  structs.file = std::move(std::ifstream(path, std::ifstream::in | std::ifstream::binary));
+  if (!structs.file) {
+    Log.error("failed to open file: {}", fs_path_to_string(path));
     return std::nullopt;
   }
 
-  if (path.filename() == "tex1_224x29_175aea04816c34a7_2.png") {
-    Log.error("Failed to allocate spng context!");
-
-  }
-
-  const SpngContext context(spng_ctx_new(0));
-  if (!context) {
-    Log.error("Failed to allocate spng context!");
+  structs.pStruct = png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
+  if (!structs.pStruct) {
+    Log.error("png_create_read_struct failed");
     return std::nullopt;
   }
 
-  if (spng_set_png_buffer(context, bytes->data(), bytes->size())) {
-    Log.error("Failed to set png buffer!");
+  structs.pInfo = png_create_info_struct(structs.pStruct);
+  if (!structs.pInfo) {
+    Log.error("png_create_info_struct failed");
     return std::nullopt;
   }
 
-  spng_ihdr ihdr{};
+  // I'm scared of putting any locals after that setjmp.
+  std::vector<png_bytep> rowPointers;
+  ByteBuffer imageData{};
+  png_uint_32 width, height;
+  int bit_depth, color_type, interlace_type, compression_type, filter_type;
+  size_t rowBytes;
+  int i;
 
-  if (spng_get_ihdr(context, &ihdr)) {
-    Log.error("Failed to get IHDR buffer!");
+  if (setjmp(png_jmpbuf(structs.pStruct))) {
+    Log.error("libpng encountered an error");
     return std::nullopt;
   }
 
-  size_t requiredSize;
-  if (spng_decoded_image_size(context, SPNG_FMT_RGBA8, &requiredSize)) {
-    Log.error("Failed to calculate image size!");
+  png_set_read_fn(structs.pStruct, &structs, readPngData);
+  png_read_info(structs.pStruct, structs.pInfo);
+
+  if (!png_get_IHDR(structs.pStruct, structs.pInfo, &width, &height, &bit_depth, &color_type, &interlace_type, &compression_type, &filter_type)) {
+    Log.error("libpng unable to read IHDR");
     return std::nullopt;
   }
 
-  ByteBuffer data{requiredSize};
-  if (spng_decode_image(context, data.data(), data.size(), SPNG_FMT_RGBA8, SPNG_DECODE_TRNS)) {
-    Log.error("Failed to decode image!");
-    return std::nullopt;
+  // Always read as RGBA8.
+  png_set_gray_to_rgb(structs.pStruct);
+  png_set_filler(structs.pStruct, 0xFF, PNG_FILLER_AFTER);
+  png_set_expand(structs.pStruct);
+  png_set_strip_16(structs.pStruct);
+
+  png_read_update_info(structs.pStruct, structs.pInfo);
+  rowBytes = png_get_rowbytes(structs.pStruct, structs.pInfo);
+  rowPointers.resize(height);
+
+  imageData.append_zeroes(rowBytes * height);
+
+  for (i = 0; i < height; i++) {
+    rowPointers[i] = imageData.data() + i * rowBytes;
   }
+
+  png_read_image(structs.pStruct, rowPointers.data());
+  png_read_end(structs.pStruct, nullptr);
 
   return ConvertedTexture{
     .format = wgpu::TextureFormat::RGBA8Unorm,
-    .width = ihdr.width,
-    .height = ihdr.height,
+    .width = width,
+    .height = height,
     .mips = 1,
-    .data = std::move(data)
+    .data = std::move(imageData)
   };
 }
 }
